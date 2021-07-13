3d398879b0580ea84403ecc7e524e7c87d224663
orekentaro <kntru0218gj@gmail.com>
2021-07-13 09:36:25 +0900

    レイアウト完成

--- models/main_model.py
+++ models/main_model.py
@@ -3,13 +3,13 @@ from models.base_model import BaseModel
 
 class MainModel(BaseModel):
   def top_page(self):
-    return render_template('main/top_page.html')
+    return render_template('main/top_page.html', home=True)
 
   def user_search(self):
-    return render_template('main/user_search.html')
+    return render_template('main/user_search.html', user_search=True)
 
   def tweet_search(self):
-    return render_template('main/tweet_search.html')
+    return render_template('main/tweet_search.html', tweet_search=True)
 
   def tweet_out(self):
-    return render_template('main/tweet_out.html')
+    return render_template('main/tweet_out.html', tweet_out=True)
