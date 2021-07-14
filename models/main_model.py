38d0acc677745848687125fd67e586ace4e65882
orekentaro <kntru0218gj@gmail.com>
2021-07-14 12:15:40 +0900

    取得モーション

--- models/main_model.py
+++ models/main_model.py
@@ -46,61 +46,65 @@ class MainModel(BaseModel):
     """
     target = request.form['target']
     count = request.form['count']
-
-    with self.start_transaction(False) as tx:
-      """まず検索条件をインサート
-      """
-      #検索条件のシーケンス
-      sql = "SELECT nextval('search_no_seq') as search_no_seq"
-      search_no_seq = tx.find_one(sql)['search_no_seq']
-
-      #検索条件保存
-      search_condition(search_no_seq, target, '0')
-
-      """取得ツイートを変数に
-      """
-      tweets = tweet_gets(target, count)
-
-      for tweet in tweets:
-        '''for文でそれぞれのデータをインサート
-        '''
-        user = tweet.user  # ユーザー情報
-
-        """ユーザー情報をテーブルに
+    
+    try:
+      with self.start_transaction(False) as tx:
+        """まず検索条件をインサート
         """
-        #ユーザーシーケンス
-        sql = "SELECT nextval('get_user_no_seq') as get_user_no_seq"
-        get_user_no_seq = tx.find_one(sql)['get_user_no_seq']
+        #検索条件のシーケンス
+        sql = "SELECT nextval('search_no_seq') as search_no_seq"
+        search_no_seq = tx.find_one(sql)['search_no_seq']
 
-        #ユーザー情報保存
-        save_user(get_user_no_seq, user)
+        #検索条件保存
+        search_condition(search_no_seq, target, '0')
 
-
-        """ツイート情報をテーブルに
+        """取得ツイートを変数に
         """
-        #ツイート番号Seq
-        sql = "SELECT nextval('tweet_id_seq') as tweet_id_seq"
-        tweet_id_seq = tx.find_one(sql)['tweet_id_seq']
+        tweets = tweet_gets(target, count)
 
-        #ツイート内容保存
-        save_tweet(tweet_id_seq, search_no_seq, user, tweet)
+        for tweet in tweets:
+          '''for文でそれぞれのデータをインサート
+          '''
+          user = tweet.user  # ユーザー情報
+
+          """ユーザー情報をテーブルに
+          """
+          #ユーザーシーケンス
+          sql = "SELECT nextval('get_user_no_seq') as get_user_no_seq"
+          get_user_no_seq = tx.find_one(sql)['get_user_no_seq']
+
+          #ユーザー情報保存
+          save_user(get_user_no_seq, user)
 
 
-        for hashtag in tweet.entities['hashtags']:
-          if hashtag:
-            '''投稿ツイートにハッシュタグがあれば
-            '''
-            # ハッシュタグ取り出す
-            hashtag = hashtag['text']
+          """ツイート情報をテーブルに
+          """
+          #ツイート番号Seq
+          sql = "SELECT nextval('tweet_id_seq') as tweet_id_seq"
+          tweet_id_seq = tx.find_one(sql)['tweet_id_seq']
 
-            #ハッシュタグSeq
-            sql = "SELECT nextval('tag_id_seq') as tag_id_seq"
-            tag_id_seq = tx.find_one(sql)['tag_id_seq']
+          #ツイート内容保存
+          save_tweet(tweet_id_seq, search_no_seq, user, tweet)
 
-            #ハッシュタグ保存
-            save_hashtag(tag_id_seq, hashtag, tweet_id_seq)
-    flash(f"ツイートを取得しました!　（{target}:{count}件）", "alert-primary")
-    return redirect(url_for('main_route.tweet_search'))
+
+          for hashtag in tweet.entities['hashtags']:
+            if hashtag:
+              '''投稿ツイートにハッシュタグがあれば
+              '''
+              # ハッシュタグ取り出す
+              hashtag = hashtag['text']
+
+              #ハッシュタグSeq
+              sql = "SELECT nextval('tag_id_seq') as tag_id_seq"
+              tag_id_seq = tx.find_one(sql)['tag_id_seq']
+
+              #ハッシュタグ保存
+              save_hashtag(tag_id_seq, hashtag, tweet_id_seq)
+      flash(f"ツイートを取得しました!　（{target}:{count}件）", "alert-primary")
+      return redirect(url_for('main_route.tweet_search'))
+    except:
+      flash(f"ツイートを取得できませんでした。", "alert-danger")
+      return redirect(url_for('main_route.tweet_search'))
 
   def get_tweet_details(self, id):
     """ツイート検索画面アンド一覧
