867e6bcf6c3d989195bbe48718fd26943d387ba1
orekentaro <kntru0218gj@gmail.com>
2021-07-25 17:11:17 +0900

    ユーザーと時間

--- user-get.py
+++ user-get.py
@@ -18,10 +18,10 @@ Twitterオブジェクトの生成
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-
+my_info = api.me()
 Account = "@Eternal_door"  # 取得したいユーザーのユーザーIDを代入
 tweets = api.user_timeline(Account, count=1, page=1)
-num = 1  # ツイート数を計算するための変数
+# num = 1  # ツイート数を計算するための変数
 # for tweet in tweets:
   # print('アカウント名 : ', tweet.user.screen_name)  # ユーザー名
   # print('投稿日時 : ', tweet.created_at)      # 呟いた日時
@@ -30,6 +30,19 @@ num = 1  # ツイート数を計算するための変数
   # print('RT : ', tweet.retweet_count)  # ツイートのリツイート数
   # print('ツイート数 : ', num)  # ツイート数
   # print('=' * 80)  # =を80個表示
-  # num += 1  # ツイート数を計算
-print(tweets[0].created_at + datetime.timedelta(hours=9))
+  # num += 1  # ツイート数を計算ß
+# print(tweets[0].created_at + datetime.timedelta(hours=9))
 
+friends_ids = []
+# フォローした人のIDを全取得
+# Cursor使うとすべて取ってきてくれるが，配列ではなくなるので配列に入れる
+for friend_id in tweepy.Cursor(api.friends_ids, user_id=my_info.id).items():
+  friends_ids.append(friend_id)
+for i in range(1):
+  user = api.get_user(friends_ids[i])
+  account = f'@{user.screen_name}'
+  tweets = api.user_timeline(account, count=1)
+  for tweet in tweets:
+    print(account, tweet.created_at)
+
+  
